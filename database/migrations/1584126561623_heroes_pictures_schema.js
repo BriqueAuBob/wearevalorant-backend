7052d051c9ca0820a32907a079a1cff378d7241f
WabElH4x0r <wabelgaming@gmail.com>
2020-03-26 15:03:36 +0100

    test

--- database/migrations/1584126561623_heroes_pictures_schema.js
+++ database/migrations/1584126561623_heroes_pictures_schema.js
@@ -7,7 +7,7 @@ class HerosPicturesSchema extends Schema {
   up () {
     this.create('hero_pictures', (table) => {
       table.increments()
-      table.integer('hero_id').unsigned().references('id').inTable('heros').onDelete('CASCADE')
+      table.integer('hero_id').unsigned().references('id').inTable('heros')
       table.text('url')
       table.timestamps()
     })
